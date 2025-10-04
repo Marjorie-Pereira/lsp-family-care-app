6ce3c877d9edc482b7d16dbdea26f5b91d698f94
Marjorie-Pereira <marjorie300904@gmail.com>
2025-10-04 18:43:24 -0300

    sign up page

--- app/signUp.tsx
+++ app/signUp.tsx
@@ -2,6 +2,7 @@ import { useRouter } from 'expo-router';
 import { useRef, useState } from 'react';
 import { Alert, Pressable, StatusBar, StyleProp, StyleSheet, Text, TextStyle, View, ViewStyle } from 'react-native';
 import Mail from '../assets/icons/Mail';
+import User from '../assets/icons/User';
 import ViewIcon from '../assets/icons/ViewIcon';
 import ViewIconSlash from '../assets/icons/ViewIconSlash';
 import Button from '../components/Button';
@@ -10,10 +11,11 @@ import ScreenWrapper from '../components/ScreenWrapper';
 import { theme } from '../constants/theme';
 import { supabase } from '../lib/supabase';
 
-const Login = () => {
+const SignUp = () => {
   const router = useRouter();
   const emailRef = useRef("");
   const passwordRef = useRef("");
+  const nameRef = useRef("");
   const [loading, setLoading] = useState(false)
   const [eyeIcon, setEyeIcon] = useState("hide")
 
@@ -23,40 +25,53 @@ const Login = () => {
       return;
     }
 
+    let name = nameRef.current.trim()
     let email = emailRef.current.trim()
     let password = passwordRef.current.trim()
-  
+
     setLoading(true)
-    const { error } = await supabase.auth.signInWithPassword({
+
+    const { error } = await supabase.auth.signUp({
       email,
-      password
+      password,
+      options: {
+        data: {
+          name
+        }
+      }
+      
     });
     setLoading(false)
     
     if(error) {
-      Alert.alert('Login', error.message)
+      Alert.alert('Sign Up', error.message)
       
-      return
     }
   }
   return (
-    <ScreenWrapper>
+    <ScreenWrapper >
       <StatusBar barStyle={"dark-content"}/>
       <View style={styles.container as StyleProp<ViewStyle>}>
+        
 
         {/* title */}
         <View>
-          <Text style={styles.welcomeText}>Bem-vindo de volta!</Text>
+          <Text style={styles.welcomeText as StyleProp<TextStyle>}>Crie sua conta</Text>
         </View>
 
         {/* form */}
         <View style={styles.form as StyleProp<ViewStyle>}>
           <Text style={{fontSize: 20, color: theme.colors.text}}>
-            Por favor insira suas credenciais
+            Por favor crie uma conta para continuar
           </Text>
 
           <Input 
-            placeholder="Endereço de email"
+            placeholder="Seu nome aqui"
+            icon={<User />}
+            onChangeText={(value: string) => nameRef.current = value}
+          />
+          <Input 
+            placeholder="Endereco de email"
             icon={<Mail />}
             onChangeText={(value: string) => emailRef.current = value}
           />
@@ -67,20 +82,18 @@ const Login = () => {
             togglePassword={() => {
               setEyeIcon(eyeIcon === "hide" ? "show" : "hide")
             }}
-            type="password"
             secureTextEntry={eyeIcon === "hide" ? true : false}
+            type="password"
           />
-          <Text style={styles.forgotPassword}>
-            Esqueceu a senha?
-          </Text>
-          <Button title='Login' loading={loading} onPress={onSubmit} />
+          
+          <Button title='Cadastrar' loading={loading} onPress={onSubmit} />
           
         </View>
         {/* footer */}
         <View style={styles.footer as StyleProp<ViewStyle>}>
-          <Text style={styles.footerText as StyleProp<TextStyle>}>Não possui uma conta?  </Text>
-          <Pressable onPress={() => router.push("./signUp")}>
-            <Text style={[styles.footerText, {color: theme.colors.primary, fontWeight: theme.fonts.semibold}] as StyleProp<TextStyle>}>Cadastre-se</Text>
+          <Text style={styles.footerText as StyleProp<TextStyle>} >Já tem uma conta? </Text>
+          <Pressable onPress={() => router.push("./login")}>
+            <Text style={[styles.footerText, {color: theme.colors.primary, fontWeight: theme.fonts.semibold}] as StyleProp<TextStyle>}>Entrar</Text>
           </Pressable>
         </View>
       </View>
@@ -88,13 +101,13 @@ const Login = () => {
   )
 }
 
-export default Login
+export default SignUp
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
         gap: 45,
-        paddingHorizontal: 20
+        paddingHorizontal: 5
     },
     welcomeText: {
       fontSize: 20,
